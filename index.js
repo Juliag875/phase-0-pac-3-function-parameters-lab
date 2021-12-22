ac523035e04ca39750fb4c85c1ea323a551f1e3b
Iuliia El Sammak <juliag875@gmail.com>
2021-12-22 11:04:06 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function introductionWithLanguage(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-// function introductionWithLanguageOptional(name, language = "JavaScript") {
-//     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-// }
+/* function introductionWithLanguageOptional(name, language = "JavaScript") {
+     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+} */
 
 function introductionWithLanguageOptional(name, language = "JavaScript") {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
